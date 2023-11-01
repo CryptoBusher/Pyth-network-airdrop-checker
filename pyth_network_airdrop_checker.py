532bb8e798ff52ed9f38710bdfa9fb4db548ef26
Crypto B <103379312+CryptoBusher@users.noreply.github.com>
2023-11-01 17:05:12 +0200

    Add files via upload

---
+++ pyth_network_airdrop_checker.py
@@ -0,0 +1,37 @@
+import requests
+from time import sleep
+
+
+def check_airdrop(_wallet: str, proxy: str = None) -> str:
+    url = f'https://airdrop.pyth.network/api/grant/v1/evm_breakdown?identity={_wallet}'
+
+    if proxy:
+        _proxies = {
+            "http": proxy,
+            "https": proxy
+        }
+        response = requests.get(url, proxies=_proxies)
+    else:
+        response = requests.get(url)
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    with open("wallets.txt", "r") as file:
+        wallets = [w.strip() for w in file]
+
+    with open("proxies.txt", "r") as file:
+        proxies = [p.strip() for p in file]
+
+
+
+    for i, wallet in enumerate(wallets):
+        try:
+            check_airdrop(wallet, proxies[i])
+        except IndexError:
+            check_airdrop(wallet)
+        except Exception as e:
+            print(f'Failed to check wallet {wallet}, reason: {e}')
+        finally:
+            sleep(1)
